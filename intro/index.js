47768ece90172b9ae7b94b330cabe41c2d006fb5
Charmee Paneliya <cpjstudy28@gmail.com>
2026-01-28 11:02:27 +0530

    Add javaScript intro

---
+++ intro/index.js
@@ -0,0 +1,9 @@
+ 
+ 
+ document.getElementById("login").innerHTML =" Hello World";
+
+        document.getElementById("btn").addEventListener("click", function myFunction(){
+            document.getElementById("login").innerHTML = "Welcome Back";
+        });
+
+       
