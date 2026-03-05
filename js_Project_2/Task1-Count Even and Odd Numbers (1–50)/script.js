78f6aa831520cb18881452f1a3d3f2915959912c
Charmee Paneliya <cpjstudy28@gmail.com>
2026-03-05 12:42:03 +0530

    add Task1

---
+++ js_Project_2/Task1-Count Even and Odd Numbers (1–50)/script.js
@@ -0,0 +1,17 @@
+document.getElementById("form").addEventListener("submit",function (e) {
+    e.preventDefault();
+
+    let evenCount = 0;
+    let oddCount = 0;
+
+    for(let i=1;i<=50;i++){
+        if(i % 2===0){
+            evenCount++;
+        }else{
+            oddCount++;
+        }
+    }
+
+    document.getElementById("even").innerHTML="Even Numbers: " + evenCount;
+    document.getElementById("odd").innerHTML="Odd Numbers: " + oddCount;
+})
