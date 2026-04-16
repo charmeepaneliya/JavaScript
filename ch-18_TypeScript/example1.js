b90ab1cbd4e7f56bd1b3e9d36b94824b64279084
Charmee Paneliya <cpjstudy28@gmail.com>
2026-04-16 15:20:22 +0530

    typescript

---
+++ ch-18_TypeScript/example1.js
@@ -0,0 +1,9 @@
+"use strict";
+let Name = "charmee";
+let num = "20";
+let num1 = 10;
+let num2 = 20;
+function add(a, b) {
+    return a + b;
+}
+console.log(add(num1, num2));
---
+++ concept_js/01_code-execution.js
@@ -0,0 +1,12 @@
+
+var num =2;
+
+function square(number){
+    var result = number * number;
+    return result;
+}
+var square1 = square(num);
+var square2 = square(7);
+
+console.log(square1);
+console.log(square2);
